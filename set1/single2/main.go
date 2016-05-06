85736901fb36f9a27e18a827b25f40f5b6174c50
Freddie Rice <freddie.rice@gmail.com>
2016-05-06 00:45:21 -0400

    Fixed single1 and made single2

--- set1/single2/main.go
+++ set1/single2/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"bufio"
 	"encoding/hex"
-	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"sort"
 	"strings"
 )
 
+// pulled from wikipedia Letter_frequency
 var KEYS = map[byte]int{
 	'e': 127,
 	't': 90,
@@ -25,6 +26,8 @@ var KEYS = map[byte]int{
 	'l': 40,
 	'u': 27,
 }
+
+const TRIES = 5
 
 type ScoredPlaintext struct {
 	Plaintext []byte
@@ -41,21 +44,22 @@ func (a ByScore) Less(i, j int) bool { return a[i].Score < a[j].Score }
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
-	ciphertext, err := readHex(reader)
-	if err != nil {
-		log.Fatalf("could not decode string as hex : %v", err)
+	ciphertexts := [][]byte{}
+	for {
+		// break if the last one was read
+		ciphertext, err := readHex(reader)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("could not decode string as hex : %v", err)
+		}
+		ciphertexts = append(ciphertexts, ciphertext)
 	}
 
-	// Calculate a decryption score for each plaintext, and remember the key
-	// with the best score.
-	scoredPlaintexts := make([]ScoredPlaintext, 0xff)
-	for i := 0; i < 0xff; i++ {
-		plaintext := make([]byte, len(ciphertext))
-		decrypt(plaintext, ciphertext, byte(i+1))
-
-		scoredPlaintexts[i].Plaintext = plaintext
-		scoredPlaintexts[i].Key = byte(i + 1)
-		scoredPlaintexts[i].Score = score(plaintext)
+	scoredPlaintexts := make([]ScoredPlaintext, TRIES*len(ciphertexts))
+	for i, c := range ciphertexts {
+		getGuesses(c, scoredPlaintexts[i*TRIES:], TRIES)
 	}
 
 	// sort them by their score
@@ -67,6 +71,22 @@ func main() {
 	for i := 0; i < top; i++ {
 		fmt.Printf("%c: %s\n", scoredPlaintexts[i].Key, string(scoredPlaintexts[i].Plaintext))
 	}
+}
+
+// Calculate a decryption score for each plaintext, then return the
+// top scoring plaintexts
+func getGuesses(ciphertext []byte, topPlaintexts []ScoredPlaintext, n int) {
+	scoredPlaintexts := make([]ScoredPlaintext, 0xff)
+	for i := 0; i < 0xff; i++ {
+		plaintext := make([]byte, len(ciphertext))
+		decrypt(plaintext, ciphertext, byte(i+1))
+
+		scoredPlaintexts[i].Plaintext = plaintext
+		scoredPlaintexts[i].Key = byte(i + 1)
+		scoredPlaintexts[i].Score = score(plaintext)
+	}
+	sort.Sort(sort.Reverse(ByScore(scoredPlaintexts)))
+	copy(topPlaintexts, scoredPlaintexts[:n])
 }
 
 func decrypt(plaintext, ciphertext []byte, key byte) {
@@ -88,7 +108,6 @@ func score(plaintext []byte) int {
 func readHex(reader *bufio.Reader) (bytes []byte, err error) {
 	str, err := reader.ReadString('\n')
 	if err != nil {
-		err = errors.New("no hex string entered")
 		return
 	}
 	str = strings.Trim(str, "\n")
